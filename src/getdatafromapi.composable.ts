be740dde69cd94362edfa8a1342631edfcf7b332
mninozebra <mni@nozebra.dk>
2022-05-10 18:04:45 +0200

    Testing some stuff

--- src/getdatafromapi.composable.ts
+++ src/getdatafromapi.composable.ts
@@ -5,24 +5,25 @@ import { ref } from 'vue';
 export function getMovieData() {
   // state encapsulated and managed by the composable
 
-  const searchApiUrl = ref('');
   const searchText = ref('');
   const errorText = ref('');
   const movieData = ref(null);
 
   // a composable can update its managed state over time.
   function getMovieFromApi(searchText) {
-    this.searchApiUrl = 'https://www.omdbapi.com/?apikey=89ea98eb&t=';
+    var searchApiUrl = 'https://www.omdbapi.com/?apikey=89ea98eb&t=';
 
-    fetch(this.searchApiUrl + this.searchText)
+    fetch(searchApiUrl + this.searchText)
       .then((response) => response.json())
       .then((data) => (this.movieData = data))
       .catch((error) => {
         console.log(error);
         this.errorText = error;
       });
+
+    return searchApiUrl;
   }
 
   // expose managed state as return value
-  return { searchApiUrl, getMovieFromApi, searchText, movieData };
+  return { getMovieFromApi, searchText, movieData };
 }
