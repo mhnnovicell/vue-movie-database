e114357702c382b4add74f5bd0abd62a7823a8bd
mninozebra <mni@nozebra.dk>
2022-05-10 19:13:39 +0200

    Trying some more stuff

--- src/getdatafromapi.composable.ts
+++ src/getdatafromapi.composable.ts
@@ -9,17 +9,18 @@ export function getMovieData() {
 
   let searchText = ref('');
   let movieData = ref(null);
-  let searchApiUrl = ref('');
   // a composable can update its managed state over time.
   function getMovieFromApi(searchText) {
     let self = this;
-    this.searchApiUrl = 'https://www.omdbapi.com/?apikey=89ea98eb&t=';
+    var searchApiUrl = 'https://www.omdbapi.com/?apikey=89ea98eb&t=';
 
-    axios.get(self.searchApiUrl + self.searchText).then((response) => {
+    axios.get(searchApiUrl + self.searchText).then((response) => {
       self.movieData = response.data;
     });
+
+    return searchApiUrl;
   }
 
   // expose managed state as return value
-  return { getMovieFromApi, searchText, movieData, searchApiUrl };
+  return { getMovieFromApi, searchText, movieData };
 }
